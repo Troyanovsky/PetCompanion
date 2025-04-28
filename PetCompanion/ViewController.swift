ff464763a82b84ae035371cf623fc0176c1374a3
troyanovsky <524619251@qq.com>
2025-04-28 16:59:24 +0800

    Initial Commit

---
+++ PetCompanion/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  PetCompanion
+//
+//  Created by Guodong Zhao on 4/28/25.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
